480f9f4b0002278295ea3e11719cd7cb85429845
Tanner Iverson <54693798+bagelbomb@users.noreply.github.com>
2019-09-20 15:13:26 -0700

    modified bio text, implemented data binding

--- app/src/main/java/com/example/aboutme/MainActivity.java
+++ app/src/main/java/com/example/aboutme/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.aboutme;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.app.Activity;
 import android.content.Context;
@@ -12,34 +13,52 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.aboutme.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
-    Button doneButton;
-    EditText editText;
-    TextView nicknameTextView;
+    private ActivityMainBinding binding;
+
+    //Button doneButton;
+    //EditText editText;
+    //TextView nicknameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        doneButton = findViewById(R.id.done_button);
-        editText = findViewById(R.id.nickname_edit);
-        nicknameTextView = findViewById(R.id.nickname_text);
+        //doneButton = findViewById(R.id.done_button);
+        //editText = findViewById(R.id.nickname_edit);
+        //nicknameTextView = findViewById(R.id.nickname_text);
 
         // Needs Java 8 (1.8) compatibility
-        doneButton.setOnClickListener(e -> {
-            addNickname(doneButton);
+//        doneButton.setOnClickListener(e -> {
+//            addNickname(doneButton);
+//        });
+
+        binding.doneButton.setOnClickListener(e -> {
+            addNickname(binding.doneButton);
         });
 
     }
 
     private void addNickname(Button doneButton) {
-        nicknameTextView.setText(editText.getText());
 
-        editText.setVisibility(View.GONE);
-        doneButton.setVisibility(View.GONE);
-        nicknameTextView.setVisibility(View.VISIBLE);
+        // Looks like I can't do the .apply in Java
+        //binding.apply {
+            binding.nicknameText.setText(binding.nicknameEdit.getText());
+            binding.nicknameEdit.setVisibility(View.GONE);
+            binding.doneButton.setVisibility(View.GONE);
+            binding.nicknameText.setVisibility(View.VISIBLE);
+            binding.invalidateAll();
+        //}
+
+//        nicknameTextView.setText(editText.getText());
+//        editText.setVisibility(View.GONE);
+//        doneButton.setVisibility(View.GONE);
+//        nicknameTextView.setVisibility(View.VISIBLE);
 
         // hide the keyboard
         InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
