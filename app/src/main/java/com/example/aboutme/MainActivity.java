4196ae1b7460d275e6a7ee6337fd763a390f7d35
Tanner Iverson <54693798+bagelbomb@users.noreply.github.com>
2019-09-19 20:25:37 -0700

    created Done button and text view with styles, implemented nickname button functionality

--- app/src/main/java/com/example/aboutme/MainActivity.java
+++ app/src/main/java/com/example/aboutme/MainActivity.java
@@ -3,12 +3,39 @@ package com.example.aboutme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button doneButton;
+    EditText editText;
+    TextView nicknameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        doneButton = findViewById(R.id.done_button);
+        editText = findViewById(R.id.nickname_edit);
+        nicknameTextView = findViewById(R.id.nickname_text);
+
+        // Needs Java 8 (1.8) compatibility
+        doneButton.setOnClickListener(e -> {
+            addNickname(doneButton);
+        });
+
     }
+
+    private void addNickname(Button doneButton) {
+        nicknameTextView.setText(editText.getText());
+
+        editText.setVisibility(View.GONE);
+        doneButton.setVisibility(View.GONE);
+        nicknameTextView.setVisibility(View.VISIBLE);
+    }
+
 }
